3f19764827b92ab3fb0d835115690e0a2924076c
KasperFyhn <kasperfyhnjacobsen@gmail.com>
2025-07-02 20:03:56 +0200

    Fix: DB sessions not getting closed after use in backend

--- narrativegraph/server/routes/common.py
+++ narrativegraph/server/routes/common.py
@@ -1,8 +1,14 @@
+from typing import Generator
+
 from fastapi import Request
 from sqlalchemy.orm import Session
 
 from narrativegraph.db.engine import get_session
 
 
-def get_db_session(request: Request) -> Session:
-    return get_session(request.app.state.db_engine)
+def get_db_session(request: Request) -> Generator[Session, None, None]:
+    db = get_session(request.app.state.db_engine)
+    try:
+        yield db
+    finally:
+        db.close()
