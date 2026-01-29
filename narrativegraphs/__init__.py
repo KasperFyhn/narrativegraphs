fbd55b72ec389ee3032977f207db9ba7e035ac6a
KasperFyhn <kasperfyhnjacobsen@gmail.com>
2026-01-29 09:20:18 +0100

    Changing release workflow

--- narrativegraphs/__init__.py
+++ narrativegraphs/__init__.py
@@ -2,3 +2,8 @@ from narrativegraphs.dto.filter import GraphFilter
 from narrativegraphs.narrativegraph import NarrativeGraph
 
 __all__ = ["NarrativeGraph", "GraphFilter"]
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
--- scripts/publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-rm dist/*
-
-./scripts/build.sh
-
-python3 -m twine upload dist/*
